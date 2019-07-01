4ba53e5d1bd6c0c2958d1c91d459d1da142a6d29
andermelo <anderson.90@gmail.com>
2019-07-01 07:35:00 -0300

    Update main.dart

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -5,7 +5,12 @@ void main(){
 
   runApp(MaterialApp(
     title: "Frases do dia",
-    home: Container(color: Colors.white,),    
+    home: Row(children: <Widget>[
+      Text(" t1 "),
+      Text(" t2 "),
+      Text(" t3 ")
+    ],),
+    //home: Container(color: Colors.white,),    
   ));
 
 }
