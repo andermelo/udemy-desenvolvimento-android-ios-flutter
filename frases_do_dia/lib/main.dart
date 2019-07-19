351f605621f1cca1faf06f66c9a1e50dc17ff5b9
andermelo <anderson.90@gmail.com>
2019-07-18 22:14:02 -0300

    updates sobre Widgets - Stateless & Stateful

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -3,17 +3,47 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false, // retirar aviso debug
-    title: "Frases do dia",
-    home: Container(
-      margin: EdgeInsets.only(top: 40),
-      decoration: BoxDecoration(
-        border: Border.all(width: 3, color: Colors.white)
-      ),
-      // color: Colors.white,
-      child: Image.asset(
-        "images/mesa.jpg",
-        fit: BoxFit.scaleDown,
-      ),
-    ),
+    home: HomeStateful(),
   ));
 }
+
+class HomeStateful extends StatefulWidget {
+  @override
+  _HomeStatefulState createState() => _HomeStatefulState();
+}
+
+class _HomeStatefulState extends State<HomeStateful> {
+
+  var _texto = "Anderson Melo";
+
+  @override
+  Widget build(BuildContext context) {
+    
+    print("build foi chamado");
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Título do App"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              onPressed: (){
+                setState(() {
+                  _texto = "Flutter é legal"; 
+                });
+                // print("Clicado");
+              },
+              color: Colors.amber,
+              child: Text("clique aqui"),
+            ),
+            Text("Nome: $_texto ")
+          ],
+        ),
+      ),
+    );
+  }
+}
+
