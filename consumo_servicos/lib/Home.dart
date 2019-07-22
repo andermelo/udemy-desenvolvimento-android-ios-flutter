24467ef70b297328cc9a1ba95c4021420ae04b75
andermelo <anderson.90@gmail.com>
2019-07-22 16:46:41 -0300

    Exibindo retorno da requisição na interface

--- consumo_servicos/lib/Home.dart
+++ consumo_servicos/lib/Home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -9,9 +10,22 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
+  // TextEditingController _controllerCep = TextEditingController();
+
+  // Mascara de formulario: https://pub.dev/packages/flutter_masked_text
+  var _controllerCepMask = new MaskedTextController(mask: '00000-000');  
+
+  String _resultado = "Resultado";
+  
   _recuperarCep() async {
 
-    String url = "http://viacep.com.br/ws/06763040/json/";
+    // utilizando API do site http://viacep.com.br/ 
+
+
+    String cepDigitado = _controllerCepMask.text;
+    cepDigitado.replaceAll('-', '');
+
+    String url = "http://viacep.com.br/ws/${cepDigitado}/json/";
     http.Response response;
 
     response = await http.get(url);
@@ -21,13 +35,17 @@ class _HomeState extends State<Home> {
     String localidade = retorno["localidade"];
     String uf = retorno["uf"];
 
-    print("Resposta logradouro: $logradouro");
-      print("Resposta bairro: $bairro");
-      print("Resposta localidade: $localidade");
-      print("Resposta uf: $uf");
+    // print("Resposta logradouro: $logradouro");
+    // print("Resposta bairro: $bairro");
+    // print("Resposta localidade: $localidade");
+    // print("Resposta uf: $uf");
     
     // print("Resposta: " + response.statusCode.toString() );
     // print("Resposta: " + response.body );
+
+    setState(() {
+     _resultado = "$logradouro, $bairro - $localidade / $uf";
+    });
 
   }
   @override
@@ -40,13 +58,36 @@ class _HomeState extends State<Home> {
       body: Container(
         padding: EdgeInsets.all(40),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Digite o cep aqui"
+                ),
+                style: TextStyle(fontSize: 20),
+                controller: _controllerCepMask,
+              ),
+            ),
+
             RaisedButton(
               color: Colors.yellow,
-              child: Text("Clique aqui", style: TextStyle(color: Colors.green),),
+              child: Text("Buscar endereço", style: TextStyle(color: Colors.green),),
               onPressed: _recuperarCep,
             ),
-            Text("Resposta logradouro: " );
+            
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: Text(
+                _resultado, 
+                style: TextStyle(fontSize: 15,),textAlign: TextAlign.center,
+              ),
+            ),
+            
           ],
         ),
       ),
