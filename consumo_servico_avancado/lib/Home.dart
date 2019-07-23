8b6d3854bcde02731c23ed960da6174318bd6492
andermelo <anderson.90@gmail.com>
2019-07-23 00:59:33 -0300

    Trabalhando com Future

---
+++ consumo_servico_avancado/lib/Home.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  // https://blockchain.info/ticker
+
+  Future<Map> _recuperarPreco() async {
+    String url = "https://blockchain.info/ticker";
+    http.Response response = await http.get(url);
+    return json.decode(response.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return FutureBuilder<Map>(
+      future: _recuperarPreco(),
+      builder: (context, snapshot){
+        String resultado;
+        switch(snapshot.connectionState){
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            print("conexao waiting");
+            resultado = "Carregando...";
+            break;
+          case ConnectionState.active:
+          case ConnectionState.done:
+            print("conexao done");
+            if(snapshot.hasError){
+              resultado = "Erro ao carregar os dados";
+            }else{
+              double valor = snapshot.data["BRL"]["buy"];
+              resultado = "Preço do bitcoin: R\$ ${valor.toString()}";
+            }
+            break;
+        }
+        return Center(
+          child: Text(
+            resultado, 
+            style: TextStyle(
+              fontSize: 25, 
+              color: Colors.blue
+              ), 
+            textAlign: TextAlign.center,
+          ),
+        );
+      }
+    );
+
+  }
+}
---
+++ consumo_servico_avancado/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'Lista.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: Lista(),
+      debugShowCheckedModeBanner: false,
+    )
+  );
+}
