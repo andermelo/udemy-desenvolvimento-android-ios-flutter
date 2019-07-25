32c8eafa61f649cdb5db00bc3bd814c75ac78f81
andermelo <anderson.90@gmail.com>
2019-07-24 22:38:20 -0300

    Executando vídeos

--- youtube/lib/telas/Inicio.dart
+++ youtube/lib/telas/Inicio.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:youtube/Api.dart';
 import 'package:youtube/model/Video.dart';
+import 'package:flutter_youtube/flutter_youtube.dart';
 
 class Inicio extends StatefulWidget {
 
@@ -22,7 +23,7 @@ class _InicioState extends State<Inicio> {
   Widget build(BuildContext context) {
     
     return FutureBuilder<List<Video>>(
-      future: _listarVideos("widget.pesquisa"),
+      future: _listarVideos(widget.pesquisa),
       builder: (context,snapshot){
         switch(snapshot.connectionState){
           case ConnectionState.none :
@@ -40,23 +41,33 @@ class _InicioState extends State<Inicio> {
                   List<Video> videos = snapshot.data;
                   Video video = videos[ index ];
 
-                  return Column(
-                    children: <Widget>[
-                      // Text("testes" + index.toString())
-                      Container(
-                        height: 200,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: NetworkImage( video.imagem )
-                          )
+                  return GestureDetector(
+                    onTap: (){
+                      FlutterYoutube.playYoutubeVideoById(
+                        apiKey: CHAVE_YOUTUBE_API,
+                        videoId: video.id,
+                        autoPlay: true,
+                        fullScreen: true
+                      );
+                    },
+                    child: Column(
+                      children: <Widget>[
+                        // Text("testes" + index.toString())
+                        Container(
+                          height: 200,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage( video.imagem )
+                            )
+                          ),
                         ),
-                      ),
-                      ListTile(
-                        title: Text(video.titulo),
-                        subtitle: Text(video.canal),
-                      )
-                    ],
+                        ListTile(
+                          title: Text(video.titulo),
+                          subtitle: Text(video.canal),
+                        )
+                      ],
+                    ),
                   );
                 },
                 separatorBuilder: (context, index) => Divider(
