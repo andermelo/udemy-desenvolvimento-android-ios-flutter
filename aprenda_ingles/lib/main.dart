2b475ab80d1ec70004ec6af1b07a52fe4d7b0b65
Anderson Melo <anderson.90@gmail.com>
2019-07-29 01:01:07 -0300

    updates gerais

--- aprenda_ingles/lib/main.dart
+++ aprenda_ingles/lib/main.dart
@@ -13,15 +13,32 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
 
   TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      length: 3,
+      vsync: this,
+      initialIndex: 0
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Aprenda Inglês"),
         bottom: TabBar(
+          controller: _tabController,
           tabs: <Widget>[
             Tab(
               text: "Bichos",
@@ -35,8 +52,13 @@ class _HomeState extends State<Home> {
           ],
         ),
       ),
-      body: Center(
-        child: Text(""),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Center(child: Text("Vai"),),
+          Center(child: Text("Dormir"),),
+          Center(child: Text("Karai!"),),
+        ],
       ),
     );
   }
