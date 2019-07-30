8bbf4fc3cdff4b749905be16a4dfb70f7edd216e
andermelo <anderson.90@gmail.com>
2019-07-29 21:57:23 -0300

    executando sons e fim do app

--- aprenda_ingles/lib/telas/Bichos.dart
+++ aprenda_ingles/lib/telas/Bichos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 class Bichos extends StatefulWidget {
   @override
@@ -6,10 +7,70 @@ class Bichos extends StatefulWidget {
 }
 
 class _BichosState extends State<Bichos> {
+
+  AudioCache _audioCache = AudioCache(prefix: 'audios/');
+
+  _executar(String nomeAudio){
+    _audioCache.play(nomeAudio + ".mp3");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _audioCache.loadAll([
+      "cao.mp3","gato.mp3","leao.mp3","macaco.mp3","ovelha.mp3","vaca.mp3",
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+
+    // double largura = MediaQuery.of(context).size.width;
+    // double altura = MediaQuery.of(context).size.height;
+
+    return GridView.count(
+      crossAxisCount: 2,
+      // childAspectRatio: 1.3,
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
+      // scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        GestureDetector(
+          onTap: (){
+            _executar('cao');
+          },
+          child: Image.asset('assets/images/cao.png'),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar('gato');
+          },
+          child: Image.asset('assets/images/gato.png'),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar('leao');
+          },
+          child: Image.asset('assets/images/leao.png'),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar('macaco');
+          },
+          child: Image.asset('assets/images/macaco.png'),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar('ovelha');
+          },
+          child: Image.asset('assets/images/ovelha.png'),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar('vaca');
+          },
+          child: Image.asset('assets/images/vaca.png'),
+        ),            
+      ],
     );
   }
 }
--- aprenda_ingles/lib/telas/Numeros.dart
+++ aprenda_ingles/lib/telas/Numeros.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
 
 class Numeros extends StatefulWidget {
   @override
@@ -6,10 +8,65 @@ class Numeros extends StatefulWidget {
 }
 
 class _NumerosState extends State<Numeros> {
+  AudioCache _audioCache = AudioCache(prefix: 'audios/');
+
+  _executar(String nomeAudio){
+    _audioCache.play(nomeAudio + ".mp3");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _audioCache.loadAll([
+      "1.mp3","2.mp3","3.mp3","4.mp3","5.mp3","6.mp3",
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return GridView.count(
+      crossAxisCount: 2,
+      // childAspectRatio: 1.3,
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
+      // scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        GestureDetector(
+          onTap: (){
+            _executar('1');
+          },
+          child: Image.asset('assets/images/1.png'),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar('2');
+          },
+          child: Image.asset('assets/images/2.png'),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar('3');
+          },
+          child: Image.asset('assets/images/3.png'),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar('4');
+          },
+          child: Image.asset('assets/images/4.png'),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar('5');
+          },
+          child: Image.asset('assets/images/5.png'),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar('6');
+          },
+          child: Image.asset('assets/images/6.png'),
+        ),            
+      ],
     );
   }
 }
