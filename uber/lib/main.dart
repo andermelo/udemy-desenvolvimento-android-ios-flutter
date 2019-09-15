93da9c402919ea9e4bd1192fa9c799b93c90836e
andermelo <anderson.90@gmail.com>
2019-09-15 11:38:43 -0300

    interface login e cadastro

--- uber/lib/main.dart
+++ uber/lib/main.dart
@@ -2,8 +2,14 @@
 import 'package:flutter/material.dart';
 import 'telas/Home.dart';
 
+final ThemeData temaPadrao = ThemeData(
+  primaryColor: Color(0xff37474f),
+  accentColor: Color(0xff546e7a)
+);
+
 void main() => runApp(MaterialApp(
   title: "Uber",
   home: Home(),
+  theme: temaPadrao,
   debugShowCheckedModeBanner: false,
 ));
