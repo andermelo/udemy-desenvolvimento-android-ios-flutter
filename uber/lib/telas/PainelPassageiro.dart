5e9b6f78ca36c812d1d826bed564f878ce2156ef
andermelo <anderson.90@gmail.com>
2019-09-23 21:51:36 -0300

    criando interface de viagem

--- uber/lib/telas/PainelPassageiro.dart
+++ uber/lib/telas/PainelPassageiro.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
 import 'package:geolocator/geolocator.dart';
+import 'dart:io';
 
 class PainelPassageiro extends StatefulWidget {
   @override
@@ -117,12 +118,98 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
         ],
       ),      
       body: Container(
-        child: GoogleMap(
-          mapType: MapType.normal,
-          initialCameraPosition: _posicaoCamera,
-          onMapCreated: _onMapCreated,
-          myLocationEnabled: true,
-          // -23.609323, -46.768152
+        child: Stack(
+          children: <Widget>[
+            GoogleMap(
+              mapType: MapType.normal,
+              initialCameraPosition: _posicaoCamera,
+              onMapCreated: _onMapCreated,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: false, //retirar botao de encontrar minha localização
+              // -23.609323, -46.768152
+            ),
+            Positioned(
+              top: 0,
+              left: 0,
+              right:0,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(3),
+                    color: Colors.white
+                  ),
+                  child: TextField(
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      icon: Container(
+                        margin: EdgeInsets.only(left: 20),
+                        width: 10,
+                        height: 10,
+                        child: Icon(Icons.location_on, color: Colors.green,),
+                      ),
+                      hintText: "Meu local",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 15,top: 16)
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 55,
+              left: 0,
+              right:0,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(3),
+                    color: Colors.white
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      icon: Container(
+                        margin: EdgeInsets.only(left: 20),
+                        width: 10,
+                        height: 10,
+                        child: Icon(Icons.local_taxi, color: Colors.grey,),
+                      ),
+                      hintText: "Digite seu destino",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 15,top: 16)
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              right: 0,
+              left: 0,
+              bottom: 0,
+              child: Padding(
+                padding: Platform.isIOS
+                ? EdgeInsets.fromLTRB(20, 10, 20, 25)
+                : EdgeInsets.all(10),
+                child: RaisedButton(
+                    child: Text("Chamar Uber".toUpperCase(), 
+                      style: TextStyle(color: Colors.white,fontSize: 20),
+                    ),
+                    color: Color(0xff1ebbd8),
+                    padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                    onPressed: (){
+
+                    },
+                  ),
+              ),
+            )
+          ],
         ),
       ),
     );
