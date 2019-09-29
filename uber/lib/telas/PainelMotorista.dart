6d62e7b2157cf62cb8f02e8b999aaa441866ba77
andermelo <anderson.90@gmail.com>
2019-09-28 23:08:09 -0300

    Listando requisição para o motorista

--- uber/lib/telas/PainelMotorista.dart
+++ uber/lib/telas/PainelMotorista.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:uber/util/StatusRequisicao.dart';
 
 class PainelMotorista extends StatefulWidget {
   @override
@@ -8,8 +12,10 @@ class PainelMotorista extends StatefulWidget {
 
 class _PainelMotoristaState extends State<PainelMotorista> {
 
-   List<String> itensMenu = ["Configurações", "Sair"];
-
+  List<String> itensMenu = ["Configurações", "Sair"];
+  final _controller = StreamController<QuerySnapshot>.broadcast();  
+  Firestore db = Firestore.instance;
+  
   _deslogarUsuario() async{
 
     FirebaseAuth auth = FirebaseAuth.instance;
@@ -27,8 +33,46 @@ class _PainelMotoristaState extends State<PainelMotorista> {
     }
   }
 
+  Stream<QuerySnapshot> _dicionarListenerRequisicoes(){
+
+    final stream = db.collection("requisicoes")
+      .where("status", isEqualTo: StatusRequisicao.AGUARDANDO)
+      .snapshots();
+
+    stream.listen((dados){
+      _controller.add(dados);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    //adiciona listener para recuperar requisições
+    _dicionarListenerRequisicoes();
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    var mensagemCarregando = Center(
+      child: Column(
+        children: <Widget>[
+          Text("Carregando requisições"),
+          CircularProgressIndicator()
+        ],
+      ),
+    );
+
+    var mensagemSemDados = Center(
+      child: Text(
+        "Você não tem nenhuma requisição :(",
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold
+        ),
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Painel motorista"),
@@ -50,7 +94,50 @@ class _PainelMotoristaState extends State<PainelMotorista> {
           )
         ],
       ),      
-      body: Container(),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _controller.stream,
+        builder: (context, snapshot){
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return mensagemCarregando;
+              break;
+            case ConnectionState.active:
+            case ConnectionState.done:
+              if (snapshot.hasError) {
+                return Text("Erro ao carregar os dados!");
+              } else {
+                QuerySnapshot querySnapshot = snapshot.data;
+                if (querySnapshot.documents.length == 0) {
+                  return mensagemSemDados;
+                }else{
+                  return ListView.separated(
+                    itemCount: querySnapshot.documents.length,
+                    separatorBuilder: (context,indice)=>Divider(
+                      height: 2,
+                      color: Colors.grey,
+                    ),
+                    itemBuilder: (context, indice){
+                      List<DocumentSnapshot> requisicoes = querySnapshot.documents.toList();
+                      DocumentSnapshot item = requisicoes[ indice ];
+
+                      String idRequisicao = item["id"];
+                      String nomePassageiro = item["passageiro"]["nome"];
+                      String rua = item["destino"]["rua"];
+                      String numero = item["destino"]["numero"];
+
+                      return ListTile(
+                        title: Text(nomePassageiro),
+                        subtitle: Text("destino: $rua, $numero"),
+                      );
+                    }
+                  );
+                }
+              }
+              break;
+          }
+        },
+      ),
     );
   }
 }
